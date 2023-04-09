50bd2ebfc410c5dd902429c2a42b307207cb05f8
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-09 19:00:35 +0200

    veriables in JS

---
+++ 02_Variables.js
@@ -0,0 +1,32 @@
+//veriable is like a container in JavaScript.
+//we can change the type of veriable on run time. thats why JS is dynamic type language.
+
+//we can change like this
+let num = 67; //a contain 67
+console.log(num); // output 67
+num = "khazir";
+console.log(num); //output khizar
+
+//variable name cannot start with number
+// let 1name = "khizar"  this is not allowed
+
+// not to use JS reserved words
+//eg let var =7;  this is not allowed
+
+//JS is case sensitive
+let name = "khizar";
+let Name = "riaz";  //both veriables are ok
+console.log(name); //reminder name is deprecated
+console.log(Name);
+
+//difference in var let and const
+//var use before ES6 so try to avoid it
+//var is globally scope
+var a = 12;
+var b = "khizarriaz"
+var c = null;
+var d = undefined;
+ 
+
+//const use to define constant veriable
+//let use for the declaration of blocked scope veriable
