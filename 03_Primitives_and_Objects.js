e134dee2c13fe8a3a0d921be3d2df37f30ada91e
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-10 03:36:27 +0200

    Primitives in JS

---
+++ 03_Primitives_and_Objects.js
@@ -0,0 +1,21 @@
+//difference between Primitives and Objects
+//7 primitives datatypes in JS (Builtin datatypes in JS)
+//NNSSBBU
+//NULL
+//NUMBER
+//SYMBOL
+//STRING
+//BOOLEAN
+//BIGINT
+//UNDIFIEND
+let a = null;
+let b = 345;
+let c = Symbol("I am a student");
+let d = "Khizar";
+let e = true; //can be false
+let f = BigInt("12345");
+let g = undefined;
+console.log(a,b,c,d,e,f,g)
+//know the type of primitive
+console.log(typeof a,typeof b, typeof c, typeof d, typeof e, typeof f, typeof g);
+//OBJECT are key value pair data type (non Primitives datatypes)
