826b81eeaab38b9e38ad596a7d84871f9dfd6d42
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-12 15:18:52 +0200

    Conditional expression in JS

--- 07_Conditional_expressions.js
+++ 07_Conditional_expressions.js
@@ -1,10 +1,55 @@
 //condition like enter email or phone number
 //if, else if , else
-//let a = prompt("Hey whats your age")
-//var name = prompt("What is your name?");  // when you only  prompt like this it shows an error prompt is not define
 
-// to handle prompt error 
-// install command:
-// npm install prompt-sync
-// and add following command on the top of the file:
-//const prompt = require("prompt-sync")();
+let age = 31;
+
+//only if
+
+if (age > 18) {
+  console.log("this is a valid age");
+}
+
+//if , else
+
+if (age < 18) {
+  console.log("this is  the teen age");
+} else {
+  console.log("this is  not the teen age");
+}
+
+//if, else if, else
+
+if (age < 12) {
+  console.log("this is  the child age");
+} else if (age > 13 && age < 19) {
+  console.log("this is the teen age");
+} else if (age > 19 && age < 25) {
+  console.log("this is the young age");
+} else {
+  console.log("this is the mature age");
+}
+
+//switch statement
+
+switch (age) {
+  case "age<12": {
+    console.log("this is the child age switch");
+    break;
+  }
+  case "age>13 && age<19": {
+    console.log("this is the child age switch");
+    break;
+  }
+  case "age>19 && age<25": {
+    console.log("this is the child age ");
+    break;
+  }
+  default: {
+    console.log("this is the mature age");
+  }
+}
+
+//ternary operator
+
+console.log("this is the", age < 19 ? "teen age" : "mature age");
+ 
