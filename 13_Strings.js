e5bd08deafb44caac8f7e88ffd18951bc2ee428d
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-24 19:13:58 +0500

    basic string

---
+++ 13_Strings.js
@@ -0,0 +1,14 @@
+//string is collection of characters
+//two way to make string single and double qoute
+let myName = "khizar";
+let yourName = 'Ali';
+
+//access single character of string
+console.log(myName[0]);
+
+
+
+
+ 
+
+
