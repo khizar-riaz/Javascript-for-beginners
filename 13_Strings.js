48d306a07f11020bc52ab612375592448617522e
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-24 19:19:25 +0500

    template literals in  string

--- 13_Strings.js
+++ 13_Strings.js
@@ -1,14 +1,15 @@
 //string is collection of characters
 //two way to make string single and double qoute
 let myName = "khizar";
-let yourName = 'Ali';
+let yourName = "Ali";
 
 //access single character of string
 console.log(myName[0]);
 
-
-
-
- 
-
-
+//template literals
+let boy1 = "Ali";
+let boy2 = "Asad";
+//use template literals with back tick to print boy1 is friend of boy2
+let sentence = "boy1 is the frined of boy 2";
+let templateSentence = `${boy1} is a friend of ${boy2}`;
+console.log(templateSentence);
