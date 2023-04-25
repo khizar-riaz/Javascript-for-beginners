df645f4154f585024007263faddab1e45eea6a6f
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-25 23:23:40 +0500

    common string methods

---
+++ 14.String_Methods.js
@@ -0,0 +1,12 @@
+let myName = "Khizar";
+//lenght property to count character
+console.log(myName.length);
+//upper case property
+console.log(myName.toUpperCase());
+//lower case property
+console.log(myName.toLowerCase());
+//to sclie string
+//from second count charcater to 4 nut 4 is not included
+console.log(myName.slice(2,4));
+//replace part of string
+console.log(myName.replace("ar" ,"riaz"));
