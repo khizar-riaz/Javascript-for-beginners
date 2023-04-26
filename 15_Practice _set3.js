a54f011029f23a05e1f8d1873a9b6d403795a0dd
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-26 23:12:40 +0500

    string practice set q 1 to 3

---
+++ 15_Practice _set3.js
@@ -0,0 +1,12 @@
+//qno1 length count
+let str = "khazir\ ";
+console.log(str.length);
+
+//qno2 include  start switch and end switch 
+const sentnce = "The quick brown fox jumps over the lazy dog";
+const word = "fox";
+console.log(sentnce.includes(word));
+
+//qno3 to lower case
+let str2 = "THE WORLD"
+console.log (str2.toLowerCase());
