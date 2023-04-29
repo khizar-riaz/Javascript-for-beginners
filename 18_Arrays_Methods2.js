1cf5840efd323405e2158624f72653c173428bf8
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-29 15:58:14 +0500

    more array methods

--- 18_Arrays_Methods2.js
+++ 18_Arrays_Methods2.js
@@ -8,7 +8,23 @@ let num_more = [11,12,14,16,18];
 let newarray = num.concat(num_more);
 console.log(newarray);
 
-//array sorting
+//array sorting alphabetically
 let arr = [12,19,3,4,2,1,18,111,22];
 arr.sort();
 console.log(arr);
+
+
+//reverse array
+let arr2 = [2,4,6,8,10];
+console.log("normal array: ", arr2);
+arr2.reverse();
+console.log("reverse array: ", arr2);
+
+//splice and sclice 
+//splice add new array item
+arr2.splice(2,3,1021,1022,1023);  //add on index 2, remove 3 elements and add 1021,1022,1023 in place of that 3 elements
+console.log("adding new element : ", arr2);
+
+//sclice
+let arr3 = arr2.slice(2,4);
+console.log(arr3);
