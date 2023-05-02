b7733f09050ec23e1c70492d335d4094397b7266
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-05-02 22:56:59 +0500

    for of for in loops in array

--- 19_LoopsWithArrays.js
+++ 19_LoopsWithArrays.js
@@ -1,9 +1,25 @@
 let num = [1, 2, 3, 5, 6];
-for (let i = 0; i<num.length; i++)
-{
-    console.log(num[i]);
+
+//print array values  using for loop
+for (let i = 0; i < num.length; i++) {
+  console.log(num[i]);
+}
+//print array values using for each loop[]
+num.forEach((element) => {
+  console.log(element);
+});
+
+//array .from which is use to make array from a string
+let mname = "khizar";
+let arr = Array.from(mname);
+console.log(arr);
+
+//for of loop
+for (let i of num) {
+  console.log(i);
 }
 
-num.forEach((element) => {
- console.log(element);
-})
+//for in loop it will return keys not the values
+for (let j in num) {
+  console.log(j);
+}
