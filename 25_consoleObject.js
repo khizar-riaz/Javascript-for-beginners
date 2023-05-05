a8dab566cccd7283b868133665f4be08e28271b6
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-05-05 22:15:40 +0500

    console functions in JS

---
+++ 25_consoleObject.js
@@ -0,0 +1,11 @@
+//console.log(console); //you can see all the functions in console
+
+console.log("hi my name is khizar");
+console.error("hey this is an error");
+console.warn("this is the warning");
+console.assert(5>33);
+console.assert(5444>33); //it will not show anything
+console.clear() //it will clear everything
+ let obj = {a1: 1, a2: 2, a3: 3};
+console.table(obj)      //it show obj data in the form of table
+console.info("this is an information"); //log and info both will go in info section
